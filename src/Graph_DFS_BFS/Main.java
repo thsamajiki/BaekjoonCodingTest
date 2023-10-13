1045077925d05dfc6a1e491ee3699ab08d20f801
thsamajiki <ant1102@naver.com>
2023-10-14 01:26:55 +0900

    2606 바이러스 저의 풀이

--- src/Graph_DFS_BFS/Main.java
+++ src/Graph_DFS_BFS/Main.java
@@ -1,2 +1,47 @@
-package Graph_DFS_BFS;public class Main {
+package Graph_DFS_BFS;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int n, m, answer;
+    static int[][] graph;
+    static boolean[] visited;
+
+    private void DFS(int v) {
+        for (int i = 1; i <= n; i++) {
+            if (!visited[i] && graph[v][i] == 1) {
+                visited[i] = true;
+                DFS(i);
+                answer++;
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        Main main = new Main();
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+
+        n = Integer.parseInt(br.readLine());
+        m = Integer.parseInt(br.readLine());
+        graph = new int[n + 1][n + 1];
+        visited = new boolean[n + 1];
+
+        for (int i = 0; i < m; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int from = Integer.parseInt(st.nextToken());
+            int to = Integer.parseInt(st.nextToken());
+            graph[from][to] = 1;
+            graph[to][from] = 1;
+        }
+
+        visited[1] = true;
+        main.DFS(1);
+
+        System.out.println(answer);
+    }
 }
