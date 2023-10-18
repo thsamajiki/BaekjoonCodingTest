027c7a247c9b40da4172f31d0095593d0e966a9f
thsamajiki <ant1102@naver.com>
2023-10-19 00:09:34 +0900

    4963 섬의 개수 나의 풀이

--- src/Graph_DFS_BFS/Main.java
+++ src/Graph_DFS_BFS/Main.java
@@ -1,31 +1,25 @@
-package Graph_DFS_BFS;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int r, c, answer = 1;
-    static int[] dx = { -1, 0, 1, 0 };
-    static int[] dy = { 0, 1, 0, -1 };
-    static char[][] map;
-    static Map<Character, Boolean> lettersChecked;
+    static int w, h, answer;
+    static int[][] map;
+    static boolean[][] visited;
+    static int[] dx = { -1, -1, 0, 1, 1, 1, 0, -1 };
+    static int[] dy = { 0, 1, 1, 1, 0, -1, -1, -1 };
 
-    private void DFS(int x, int y, int count) {
-        for (int i = 0; i < 4; i++) {
+    private void DFS(int x, int y) {
+        for (int i = 0; i < 8; i++) {
             int nextX = x + dx[i];
             int nextY = y + dy[i];
 
-            if (nextX < 0 || nextX >= r || nextY < 0 || nextY >= c) continue;
-            if (!lettersChecked.get(map[nextX][nextY])) {
-                lettersChecked.put(map[nextX][nextY], true);
-                DFS(nextX, nextY, count + 1);
-                lettersChecked.put(map[nextX][nextY], false);
-            } else {
-                answer = Math.max(answer, count);
+            if (nextX < 0 || nextX >= h || nextY < 0 || nextY >= w) continue;
+
+            if (map[nextX][nextY] == 1 && !visited[nextX][nextY]) {
+                visited[nextX][nextY] = true;
+                DFS(nextX, nextY);
             }
         }
     }
@@ -35,28 +29,32 @@ public class Main {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        r = Integer.parseInt(st.nextToken());
-        c = Integer.parseInt(st.nextToken());
 
-        map = new char[r][c];
-        lettersChecked = new HashMap<>();
+        while ((w = Integer.parseInt(st.nextToken())) > 0 && (h = Integer.parseInt(st.nextToken())) > 0) {
+            map = new int[h][w];
+            visited = new boolean[h][w];
+            answer = 0;
 
-        for (int i = 0; i < r; i++) {
-            String temp = br.readLine();
-            for (int j = 0; j < c; j++) {
-                map[i][j] = temp.charAt(j);
+            for (int i = 0; i < h; i++) {
+                st = new StringTokenizer(br.readLine());
+                for (int j = 0; j < w; j++) {
+                    map[i][j] = Integer.parseInt(st.nextToken());
+                }
             }
+
+            for (int i = 0; i < h; i++) {
+                for (int j = 0; j < w; j++) {
+                    if (map[i][j] == 1 && !visited[i][j]) {
+                        answer++;
+                        visited[i][j] = true;
+                        main.DFS(i, j);
+                    }
+                }
+            }
+
+            System.out.println(answer);
+
+            st = new StringTokenizer(br.readLine());
         }
-
-        for (char ch = 'A'; ch <= 'Z'; ch++) {
-            lettersChecked.put(ch, false);
-        }
-
-        char firstLetter = map[0][0];
-        lettersChecked.put(firstLetter, true);
-
-        main.DFS(0, 0, 1);
-
-        System.out.println(answer);
     }
 }
