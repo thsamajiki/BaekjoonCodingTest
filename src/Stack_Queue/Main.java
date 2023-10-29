748f24b8359f0eb7a2a38868a3105acdb6976c48
thsamajiki <ant1102@naver.com>
2023-10-29 21:39:00 +0900

    1406 에디터 남의 풀이

--- src/Stack_Queue/Main.java
+++ src/Stack_Queue/Main.java
@@ -6,22 +6,52 @@ import java.io.InputStreamReader;
 import java.util.Stack;
 
 public class Main {
-    public int solution(String str) {
-        int answer = 0;
-        Stack<Character> stack = new Stack<>();
+    public String solution(String str, String[] commands) {
+        String answer = "";
+        Stack<Character> originalStack = new Stack<>(); // 기준, peek 지점이 바로 현재 커서의 위치
+        Stack<Character> tempStack = new Stack<>(); // 커서 뒤의 원소들을 tempStack으로 이동시켜둔다.
 
-        for (int i = 0; i < str.length(); i++) {
-            if (str.charAt(i) == '(') {
-                stack.push('(');
-            } else {
-                if (str.charAt(i - 1) == '(') {
-                    stack.pop();
-                    answer += stack.size();
-                } else {
-                    stack.pop();
-                    answer++;
-                }
+        int N = str.length();
+        int M = commands.length;
+
+        for (int i = 0; i < N; i++) {
+            originalStack.push(str.charAt(i));
+        }
+
+        for (int i = 0; i < M; i++) {
+            if (commands[i].length() >= 2) {
+                commands[i] = commands[i].replace(" ", "");
             }
+            char[] command = commands[i].toCharArray();
+
+            switch (command[0]) {
+                case 'L': // 커서를 왼쪽으로 옮겨야 하기 때문에 커서 뒤에 존재하는 원소들을 tempStack으로 옮긴다.
+                    if (!originalStack.isEmpty()) {
+                        tempStack.push(originalStack.pop());
+                    }
+                    break;
+                case 'D': // 커서를 오른쪽으로 옮겨야 하기 때문에 tempStack에 존재하는 원소들을 originalStack으로 옮긴다.
+                    if (!tempStack.isEmpty()) {
+                        originalStack.push(tempStack.pop());
+                    }
+                    break;
+                case 'B':
+                    if (!originalStack.isEmpty()) {
+                        originalStack.pop();
+                    }
+                    break;
+                case 'P':
+                    originalStack.push(command[1]);
+                    break;
+            }
+        }
+
+        while (!tempStack.isEmpty()) {
+            originalStack.push(tempStack.pop());
+        }
+
+        for (char x : originalStack) {
+            answer += x;
         }
 
         return answer;
@@ -33,6 +63,14 @@ public class Main {
 
         String str = br.readLine();
 
-        System.out.println(main.solution(str));
+        int M = Integer.parseInt(br.readLine());
+
+        String[] commands = new String[M];
+
+        for (int i = 0; i < M; i++) {
+            commands[i] = br.readLine();
+        }
+
+        System.out.println(main.solution(str, commands));
     }
 }
