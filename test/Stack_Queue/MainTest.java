2349b370f96db1843468b917ee791db76ed23c1b
thsamajiki <ant1102@naver.com>
2023-10-29 21:39:03 +0900

    1406 에디터 남의 풀이

--- test/Stack_Queue/MainTest.java
+++ test/Stack_Queue/MainTest.java
@@ -9,6 +9,8 @@ class MainTest {
     @Test
     void solution() {
         Main main = new Main();
-        assertEquals(17, main.solution("()(((()())(())()))(())"));
+        assertEquals("abcdyx", main.solution("abcd", new String[] { "P x", "L", "P y" }));
+        assertEquals("yxabc", main.solution("abc", new String[] { "L", "L", "L", "L", "L", "P x", "L", "B", "P y" }));
+        assertEquals("yxz", main.solution("dmih", new String[] { "B", "B", "P x", "L", "B", "B", "B", "P y", "D", "D", "P z" }));
     }
 }
