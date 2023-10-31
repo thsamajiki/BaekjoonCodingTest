d7bf1e3de7de9b6736a17439a8fe69475db5d491
thsamajiki <ant1102@naver.com>
2023-10-31 23:42:19 +0900

    9935 문자열 폭발 남의 풀이

--- test/Stack_Queue/MainTest.java
+++ test/Stack_Queue/MainTest.java
@@ -2,11 +2,14 @@ package Stack_Queue;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class MainTest {
 
     @Test
     void solution() {
         Main main = new Main();
-//        assertArrayEquals(new int[] { 0, 0, 2, 2, 4 }, main.solution(5, new int[] { 6, 9, 5, 7, 4 }));
+        assertEquals("mirkovniz", main.solution("mirkovC4nizCC44", "C4"));
+        assertEquals("FRULA", main.solution("12ab112ab2ab", "12ab"));
     }
 }
