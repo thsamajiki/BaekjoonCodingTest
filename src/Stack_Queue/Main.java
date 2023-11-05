189cc41e247166cd57162e4b8936815499b68afc
thsamajiki <ant1102@naver.com>
2023-11-06 03:43:15 +0900

    1158 요세푸스 문제 남의 풀이

--- src/Stack_Queue/Main.java
+++ src/Stack_Queue/Main.java
@@ -6,36 +6,25 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-    public int solution(int[] weights, int bridgeLength, int bridgeWeightLimit) {
-        int answer = 0;
+    public String solution(int n, int k) {
+        Queue<Integer> q = new LinkedList<>();
+        StringBuilder sb = new StringBuilder("<");
 
-        int bridgeWeightSum = 0;
-        Queue<Integer> bridges = new LinkedList<>();
-        Queue<Integer> trucks = new LinkedList<>();
-
-        for (int i = 0; i < bridgeLength; i++) {
-            bridges.offer(0);
+        for (int i = 1; i <= n; i++) {
+            q.offer(i);
         }
 
-        for (int i = 0; i < weights.length; i++) {
-            trucks.offer(weights[i]);
-        }
-
-        while (!bridges.isEmpty()) { // 더이상 넣을 트럭이 없을 때까지 반복 (반복 1번에 시간 + 1)
-            answer++;
-            bridgeWeightSum -= bridges.poll(); // 다리 무게에서 내려온 트럭 무게를 뺌
-
-            if (!trucks.isEmpty()) {
-                if (trucks.peek() + bridgeWeightSum <= bridgeWeightLimit) { // 앞의 트럭의 무게를 더해도 다리의 최대 하중보다 작다면
-                    bridgeWeightSum += trucks.peek();
-                    bridges.offer(trucks.poll()); // 다리에 트럭 추가
-                } else { // 다리의 최대 하중을 초과하면
-                    bridges.offer(0); // 다리 위에 트럭을 추가하지 않음
-                }
+        while (q.size() != 1) {
+            // k - 1까지 처음에 있던 값을 맨 뒤로 보낸다.
+            for (int i = 0; i < k - 1; i++) {
+                q.offer(q.poll());
             }
+            sb.append(q.poll()).append(", ");
         }
 
-        return answer;
+        sb.append(q.poll()).append(">");
+
+        return sb.toString();
     }
 
     public static void main(String[] args) throws IOException {
@@ -45,17 +34,9 @@ public class Main {
 
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int n = Integer.parseInt(st.nextToken()); // 다리를 건너는 트럭의 수
-        int w = Integer.parseInt(st.nextToken()); // 다리의 길이
-        int L = Integer.parseInt(st.nextToken()); // 다리의 최대 하중
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
 
-        int[] truckWeights = new int[n];
-
-        st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < n; i++) {
-            truckWeights[i] = Integer.parseInt(st.nextToken());
-        }
-
-        System.out.println(main.solution(truckWeights, w, L));
+        System.out.println(main.solution(n, k));
     }
 }
