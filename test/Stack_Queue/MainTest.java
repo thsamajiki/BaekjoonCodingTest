e414b0d1290112da88f3e8e6e3bb9276dd402ec9
thsamajiki <ant1102@naver.com>
2023-11-06 00:23:42 +0900

    3190 뱀 남의 풀이

--- test/Stack_Queue/MainTest.java
+++ test/Stack_Queue/MainTest.java
@@ -2,16 +2,11 @@ package Stack_Queue;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 class MainTest {
 
     @Test
     void solution() {
         Main main = new Main();
-        assertEquals(new ArrayList<>(List.of(1, 3, 5, 7, 4, 2, 6)), main.solution(7));
+//        assertEquals(new ArrayList<>(List.of(1, 3, 5, 7, 4, 2, 6)), main.solution(7));
     }
 }
