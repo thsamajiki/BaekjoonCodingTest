8148a8c2aa0cc2955bf7b826b2baafeb9e3ce2d9
thsamajiki <ant1102@naver.com>
2023-11-06 03:43:18 +0900

    1158 요세푸스 문제 남의 풀이

--- test/Stack_Queue/MainTest.java
+++ test/Stack_Queue/MainTest.java
@@ -9,8 +9,6 @@ class MainTest {
     @Test
     void solution() {
         Main main = new Main();
-        assertEquals(8, main.solution(new int[] { 7, 4, 5, 6 }, 2, 10));
-        assertEquals(101, main.solution(new int[] { 10 }, 100, 100));
-        assertEquals(110, main.solution(new int[] { 10, 10, 10, 10, 10, 10, 10, 10, 10, 10 }, 100, 100));
+        assertEquals("<3, 6, 2, 7, 5, 1, 4>", main.solution(7, 3));
     }
 }
