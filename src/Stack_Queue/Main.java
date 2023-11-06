9b61d87094debde7cdd04d83481476e20190279e
thsamajiki <ant1102@naver.com>
2023-11-06 18:38:05 +0900

    15828 Router 문제 남의 풀이

--- src/Stack_Queue/Main.java
+++ src/Stack_Queue/Main.java
@@ -6,37 +6,41 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-    public String solution(int n, int k) {
-        Queue<Integer> q = new LinkedList<>();
-        StringBuilder sb = new StringBuilder("<");
-
-        for (int i = 1; i <= n; i++) {
-            q.offer(i);
-        }
-
-        while (q.size() != 1) {
-            // k - 1까지 처음에 있던 값을 맨 뒤로 보낸다.
-            for (int i = 0; i < k - 1; i++) {
-                q.offer(q.poll());
-            }
-            sb.append(q.poll()).append(", ");
-        }
-
-        sb.append(q.poll()).append(">");
-
-        return sb.toString();
-    }
-
     public static void main(String[] args) throws IOException {
         Main main = new Main();
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
 
-        int n = Integer.parseInt(st.nextToken());
-        int k = Integer.parseInt(st.nextToken());
+        int bufferSize = Integer.parseInt(br.readLine());
 
-        System.out.println(main.solution(n, k));
+        Queue<Integer> q = new LinkedList<>();
+
+        while (true) {
+            int packet = Integer.parseInt(br.readLine());
+
+            if (packet == -1) {
+                break;
+            }
+
+            if (packet == 0) {
+                q.poll();
+            } else {
+                if (packet > 0 && bufferSize > q.size()) {
+                    q.offer(packet);
+                }
+            }
+        }
+
+        if (q.isEmpty()) {
+            sb.append("empty");
+        } else {
+            for (int packet : q) {
+                sb.append(packet).append(" ");
+            }
+        }
+
+        System.out.println(sb);
     }
 }
