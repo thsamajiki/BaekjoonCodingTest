eccbeef212b9c92324c67ea8c1ad7eedec6bd1e3
thsamajiki <ant1102@naver.com>
2023-11-08 18:48:00 +0900

    2839 설탕 배달 문제 남의 풀이

---
+++ test/Greedy_Algorithm/MainTest.java
@@ -0,0 +1,18 @@
+package Greedy_Algorithm;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @Test
+    void solution() {
+        Main main = new Main();
+        assertEquals(4, main.solution(18));
+        assertEquals(-1, main.solution(4));
+        assertEquals(2, main.solution(6));
+        assertEquals(3, main.solution(9));
+        assertEquals(3, main.solution(11));
+    }
+}
