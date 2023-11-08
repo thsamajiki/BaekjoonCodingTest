1d4b748d59082c2d477497205c01c9daf3013e1f
thsamajiki <ant1102@naver.com>
2023-11-08 19:19:47 +0900

    11399 ATM 문제 나의 풀이

--- test/Greedy_Algorithm/MainTest.java
+++ test/Greedy_Algorithm/MainTest.java
@@ -9,10 +9,6 @@ class MainTest {
     @Test
     void solution() {
         Main main = new Main();
-        assertEquals(4, main.solution(18));
-        assertEquals(-1, main.solution(4));
-        assertEquals(2, main.solution(6));
-        assertEquals(3, main.solution(9));
-        assertEquals(3, main.solution(11));
+        assertEquals(32, main.solution(new int[] { 3, 1, 4, 3, 2 }));
     }
 }
