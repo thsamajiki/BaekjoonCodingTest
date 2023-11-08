a27ccb359477fdfde90ed59b715d69ef8259a004
thsamajiki <ant1102@naver.com>
2023-11-08 19:19:44 +0900

    11399 ATM 문제 나의 풀이

--- src/Greedy_Algorithm/Main.java
+++ src/Greedy_Algorithm/Main.java
@@ -3,24 +3,19 @@ package Greedy_Algorithm;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
-    public int solution(int n) {
-        int answer = -1;
+    public int solution(int[] arr) {
+        int answer = 0;
 
-        int weightPerFive = n / 5;
-        int weightPerThree = 0;
+        Arrays.sort(arr);
 
-        while (weightPerFive >= 0) {
-            int remainingWeight = n - weightPerFive * 5;
-
-            if (remainingWeight % 3 == 0) {
-                weightPerThree = remainingWeight / 3;
-                answer = weightPerFive + weightPerThree;
-                break;
+        for (int i = arr.length - 1; i >= 0; i--) {
+            for (int j = i; j >= 0; j--) {
+                answer += arr[j];
             }
-
-            weightPerFive--;
         }
 
         return answer;
@@ -33,6 +28,12 @@ public class Main {
 
         int N = Integer.parseInt(br.readLine());
 
-        System.out.println(main.solution(N));
+        int[] times = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            times[i] = Integer.parseInt(st.nextToken());
+        }
+
+        System.out.println(main.solution(times));
     }
 }
