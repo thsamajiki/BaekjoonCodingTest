690e0a74c86e1acacfcbbc2cd282539f17fb6e42
thsamajiki <ant1102@naver.com>
2023-12-26 19:10:12 +0900

    1946 신입 사원 나의 풀이

--- test/Greedy_Algorithm/MainTest.java
+++ test/Greedy_Algorithm/MainTest.java
@@ -9,6 +9,20 @@ class MainTest {
     @Test
     void solution() {
         Main main = new Main();
-        assertEquals(32, main.solution(new int[] { 3, 1, 4, 3, 2 }));
+        assertEquals(4, main.solution(new Person[] {
+                new Person(3, 2),
+                new Person(1, 4),
+                new Person(4, 1),
+                new Person(2, 3),
+                new Person(5, 5) }));
+
+        assertEquals(3, main.solution(new Person[] {
+                new Person(3, 6),
+                new Person(7, 3),
+                new Person(4, 2),
+                new Person(1, 4),
+                new Person(5, 7),
+                new Person(2, 5),
+                new Person(6, 1)}));
     }
 }
