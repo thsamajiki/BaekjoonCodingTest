14751ca4d88e88238b071405ce7f08c743cb926b
thsamajiki <ant1102@naver.com>
2023-12-30 23:59:16 +0900

    14499 주사위 굴리기 타인의 풀이

--- src/Graph_DFS_BFS/Main.java
+++ src/Graph_DFS_BFS/Main.java
@@ -3,22 +3,137 @@ package Graph_DFS_BFS;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
+import java.util.*;
+
+class Dice {
+    int front;
+    int back;
+    int up;
+    int down;
+    int left;
+    int right;
+
+    public Dice() {
+        this.front = 0;
+        this.back = 0;
+        this.up = 0;
+        this.down = 0;
+        this.left = 0;
+        this.right = 0;
+    }
+
+    public void moveNorth() {
+        int tmp = this.front;
+        this.front = this.up;
+        this.up = this.back;
+        this.back = this.down;
+        this.down = tmp;
+    }
+
+    public void moveSouth() {
+        int tmp = this.down;
+        this.down = this.back;
+        this.back = this.up;
+        this.up = this.front;
+        this.front = tmp;
+    }
+
+    public void moveEast() {
+        int tmp = this.down;
+        this.down = this.right;
+        this.right = this.up;
+        this.up = this.left;
+        this.left = tmp;
+    }
+
+    public void moveWest() {
+        int tmp = this.down;
+        this.down = this.left;
+        this.left = this.up;
+        this.up = this.right;
+        this.right = tmp;
+    }
+
+    public void copyToDice(int req) {
+        this.down = req;
+    }
+
+    public void copyToMap(int[][] req, int y, int x) {
+        req[y][x] = this.down;
+    }
+}
 
 public class Main {
-    static boolean[] visited;
-    static int[] nums;
-    static ArrayList<Integer> list;
+    static int N, M, x, y, K;
+    static int[][] map;
+    static Queue<Integer> commands;
+    static Dice dice;
 
-    private void solution(int start, int target) {
-        if (!visited[nums[start]]) {
-            visited[nums[start]] = true;
-            solution(nums[start], target);
-            visited[nums[start]] = false;
+    public int solution(int command) {
+        switch (command) {
+            case 1: // 동쪽
+                if (x + 1 < M) {
+                    dice.moveEast();
+                    x++;
+
+                    if (map[y][x] == 0) {
+                        dice.copyToMap(map, y, x);
+                    } else {
+                        dice.copyToDice(map[y][x]);
+                        map[y][x] = 0;
+                    }
+
+                    return dice.up;
+                }
+                break;
+            case 2: // 서쪽
+                if (x - 1 >= 0) {
+                    dice.moveWest();
+                    x--;
+
+                    if (map[y][x] == 0) {
+                        dice.copyToMap(map, y, x);
+                    } else {
+                        dice.copyToDice(map[y][x]);
+                        map[y][x] = 0;
+                    }
+
+                    return dice.up;
+                }
+                break;
+            case 3: // 북쪽
+                if (y - 1 >= 0) {
+                    dice.moveNorth();
+                    y--;
+
+                    if (map[y][x] == 0) {
+                        dice.copyToMap(map, y, x);
+                    } else {
+                        dice.copyToDice(map[y][x]);
+                        map[y][x] = 0;
+                    }
+
+                    return dice.up;
+                }
+                break;
+            case 4: // 남쪽
+                if (y + 1 < N) {
+                    dice.moveSouth();
+                    y++;
+
+                    if (map[y][x] == 0) {
+                        dice.copyToMap(map, y, x);
+                    } else {
+                        dice.copyToDice(map[y][x]);
+                        map[y][x] = 0;
+                    }
+
+                    return dice.up;
+                }
+                break;
         }
 
-        if (nums[start] == target) list.add(target);
+        return -1;
     }
 
     public static void main(String[] args) throws IOException {
@@ -26,27 +141,39 @@ public class Main {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        y = Integer.parseInt(st.nextToken());
+        x = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
 
-        nums = new int[N + 1];
+        map = new int[21][21];
+        commands = new LinkedList<>();
+        dice = new Dice();
 
-        for (int i = 1; i <= N; i++) {
-            nums[i] = Integer.parseInt(br.readLine());
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < M; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
         }
 
-        list = new ArrayList<>();
-        visited = new boolean[N + 1];
-
-        for (int i = 1; i <= N; i++) {
-            visited[i] = true;
-            main.solution(i, i);
-            visited[i] = false;
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < K; i++) {
+            commands.add(Integer.parseInt(st.nextToken()));
         }
 
-        Collections.sort(list); // 작은 수 부터 출력하므로 정렬한다.
-        System.out.println(list.size());
-        for(int i = 0; i < list.size(); i++) {
-            System.out.println(list.get(i));
+        StringBuilder sb = new StringBuilder();
+        while (!commands.isEmpty()) {
+            int command = commands.poll();
+            int top = main.solution(command);
+            if (top != -1) {
+                sb.append(top);
+                sb.append("\n");
+            }
         }
+
+        System.out.println(sb);
     }
 }
