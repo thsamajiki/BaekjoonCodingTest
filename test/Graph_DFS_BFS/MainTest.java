d171a07f5dce902a2e29ae800a1ff98656236f88
thsamajiki <ant1102@naver.com>
2024-01-02 20:13:04 +0900

    15681 트리와 쿼리 타인의 풀이

---
+++ test/Graph_DFS_BFS/MainTest.java
@@ -0,0 +1,25 @@
+package Graph_DFS_BFS;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @Test
+    void solution() {
+        Main main = new Main();
+        Main.N = 9;
+        Main.R = 5;
+        Main.Q = 3;
+        Main.tree = new ArrayList[Main.N + 1]; // 인접 리스트로 간선의 정보를 저장
+        for (int i = 1; i < Main.N + 1; i++) {
+            Main.tree[i] = new ArrayList<>();
+        }
+        Main.subTree = new int[Main.N + 1]; // 각 노드의 서브트리
+        Main.visited = new boolean[Main.N + 1];
+        assertEquals(1, main.solution(5));
+    }
+}
