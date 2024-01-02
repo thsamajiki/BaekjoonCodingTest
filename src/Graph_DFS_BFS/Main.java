07a3392b0695465b3bdf43b46301bb26946cff56
thsamajiki <ant1102@naver.com>
2024-01-02 20:12:34 +0900

    15681 트리와 쿼리 타인의 풀이

--- src/Graph_DFS_BFS/Main.java
+++ src/Graph_DFS_BFS/Main.java
@@ -5,135 +5,22 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
-class Dice {
-    int front;
-    int back;
-    int up;
-    int down;
-    int left;
-    int right;
-
-    public Dice() {
-        this.front = 0;
-        this.back = 0;
-        this.up = 0;
-        this.down = 0;
-        this.left = 0;
-        this.right = 0;
-    }
-
-    public void moveNorth() {
-        int tmp = this.front;
-        this.front = this.up;
-        this.up = this.back;
-        this.back = this.down;
-        this.down = tmp;
-    }
-
-    public void moveSouth() {
-        int tmp = this.down;
-        this.down = this.back;
-        this.back = this.up;
-        this.up = this.front;
-        this.front = tmp;
-    }
-
-    public void moveEast() {
-        int tmp = this.down;
-        this.down = this.right;
-        this.right = this.up;
-        this.up = this.left;
-        this.left = tmp;
-    }
-
-    public void moveWest() {
-        int tmp = this.down;
-        this.down = this.left;
-        this.left = this.up;
-        this.up = this.right;
-        this.right = tmp;
-    }
-
-    public void copyToDice(int req) {
-        this.down = req;
-    }
-
-    public void copyToMap(int[][] req, int y, int x) {
-        req[y][x] = this.down;
-    }
-}
-
 public class Main {
-    static int N, M, x, y, K;
-    static int[][] map;
-    static Queue<Integer> commands;
-    static Dice dice;
+    static int N, R, Q;
+    static ArrayList<Integer>[] tree; // 인접 리스트로 간선의 정보를 저장
+    static int[] subTree; // 각 노드의 서브트리
+    static boolean[] visited;
 
-    public int solution(int command) {
-        switch (command) {
-            case 1: // 동쪽
-                if (x + 1 < M) {
-                    dice.moveEast();
-                    x++;
-
-                    if (map[y][x] == 0) {
-                        dice.copyToMap(map, y, x);
-                    } else {
-                        dice.copyToDice(map[y][x]);
-                        map[y][x] = 0;
-                    }
-
-                    return dice.up;
-                }
-                break;
-            case 2: // 서쪽
-                if (x - 1 >= 0) {
-                    dice.moveWest();
-                    x--;
-
-                    if (map[y][x] == 0) {
-                        dice.copyToMap(map, y, x);
-                    } else {
-                        dice.copyToDice(map[y][x]);
-                        map[y][x] = 0;
-                    }
-
-                    return dice.up;
-                }
-                break;
-            case 3: // 북쪽
-                if (y - 1 >= 0) {
-                    dice.moveNorth();
-                    y--;
-
-                    if (map[y][x] == 0) {
-                        dice.copyToMap(map, y, x);
-                    } else {
-                        dice.copyToDice(map[y][x]);
-                        map[y][x] = 0;
-                    }
-
-                    return dice.up;
-                }
-                break;
-            case 4: // 남쪽
-                if (y + 1 < N) {
-                    dice.moveSouth();
-                    y++;
-
-                    if (map[y][x] == 0) {
-                        dice.copyToMap(map, y, x);
-                    } else {
-                        dice.copyToDice(map[y][x]);
-                        map[y][x] = 0;
-                    }
-
-                    return dice.up;
-                }
-                break;
+    public int solution(int root) {
+        int count = 1;
+        for(int node: tree[root]) {
+            if (!visited[node]) {
+                visited[node] = true;
+                count += solution(node); // 자식노드의 개수를 더해준다.
+            }
         }
 
-        return -1;
+        return subTree[root] = count;
     }
 
     public static void main(String[] args) throws IOException {
@@ -143,37 +30,31 @@ public class Main {
 
         StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        y = Integer.parseInt(st.nextToken());
-        x = Integer.parseInt(st.nextToken());
-        K = Integer.parseInt(st.nextToken());
+        R = Integer.parseInt(st.nextToken());
+        Q = Integer.parseInt(st.nextToken());
 
-        map = new int[21][21];
-        commands = new LinkedList<>();
-        dice = new Dice();
+        tree = new ArrayList[N + 1];
+        subTree = new int[N + 1];
+        visited = new boolean[N + 1];
 
-        for (int i = 0; i < N; i++) {
+        for (int i = 1; i < N + 1; i++) {
+            tree[i] = new ArrayList<>();
+        }
+
+        for (int i = 0; i < N - 1; i++) {
             st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < M; j++) {
-                map[i][j] = Integer.parseInt(st.nextToken());
-            }
+            int from = Integer.parseInt(st.nextToken());;
+            int to = Integer.parseInt(st.nextToken());;
+            tree[from].add(to);
+            tree[to].add(from);
         }
 
-        st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < K; i++) {
-            commands.add(Integer.parseInt(st.nextToken()));
-        }
+        visited[R] = true;
+        main.solution(R);
 
-        StringBuilder sb = new StringBuilder();
-        while (!commands.isEmpty()) {
-            int command = commands.poll();
-            int top = main.solution(command);
-            if (top != -1) {
-                sb.append(top);
-                sb.append("\n");
-            }
+        for (int i = 0; i < Q; i++) {
+            int U = Integer.parseInt(br.readLine());
+            System.out.println(subTree[U]);
         }
-
-        System.out.println(sb);
     }
 }
