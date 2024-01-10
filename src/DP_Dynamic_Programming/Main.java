a74ee8b5f65aed9554ff8a0c8f329f95cf004482
thsamajiki <ant1102@naver.com>
2024-01-10 22:45:52 +0900

    2579 계단 오르기 타인의 풀이

--- src/DP_Dynamic_Programming/Main.java
+++ src/DP_Dynamic_Programming/Main.java
@@ -3,46 +3,48 @@ package DP_Dynamic_Programming;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class Main {
-    public int solution(int[][] rgbCostArr) {
-//        1번 집의 색은 2번 집의 색과 같지 않아야 한다.
-//        N번 집의 색은 N-1번 집의 색과 같지 않아야 한다.
-//        i(2 ≤ i ≤ N-1)번 집의 색은 i-1번, i+1번 집의 색과 같지 않아야 한다.
-        int answer;
-        int length = rgbCostArr.length - 1;
+    static int n;
+    static int[] stairs;
 
-        for (int i = 1; i < length; i++) {
-            int red = rgbCostArr[i + 1][0];
-            int green = rgbCostArr[i + 1][1];
-            int blue = rgbCostArr[i + 1][2];
+    public int solution(int[] stairs) {
+        int answer = 0;
+        int[] dp = new int[n + 1];
+        dp[1] = stairs[1];
 
-            rgbCostArr[i + 1][0] = Math.min(rgbCostArr[i][1], rgbCostArr[i][2]) + red;
-            rgbCostArr[i + 1][1] = Math.min(rgbCostArr[i][0], rgbCostArr[i][2]) + green;
-            rgbCostArr[i + 1][2] = Math.min(rgbCostArr[i][0], rgbCostArr[i][1]) + blue;
+        if (n >= 2) {
+            dp[2] = stairs[1] + stairs[2];
         }
 
-        answer = Math.min(Math.min(rgbCostArr[length][0], rgbCostArr[length][1]), rgbCostArr[length][2]);
+        if (n >= 3) {
+            dp[3] = Math.max(stairs[1], stairs[2]) + stairs[3];
+        }
+
+        if (n >= 4) {
+            for (int i = 4; i <= n; i++) {
+                dp[i] = Math.max(dp[i - 3] + stairs[i - 1], dp[i - 2]) + stairs[i];
+            }
+        }
+
+        answer = dp[n];
 
         return answer;
     }
 
     public static void main(String[] args) throws IOException {
         Main main = new Main();
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int N = Integer.parseInt(br.readLine()); // 집의 수
+        n = Integer.parseInt(br.readLine());
 
-        int[][] arr = new int[N + 1][3];
+        stairs = new int[n + 1];
 
-        for (int i = 1; i <= N; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            arr[i][0] = Integer.parseInt(st.nextToken());
-            arr[i][1] = Integer.parseInt(st.nextToken());
-            arr[i][2] = Integer.parseInt(st.nextToken());
+        for (int i = 1; i <= n; i++) {
+            stairs[i] = Integer.parseInt(br.readLine());
         }
 
-        System.out.println(main.solution(arr));
+        System.out.println(main.solution(stairs));
     }
 }
