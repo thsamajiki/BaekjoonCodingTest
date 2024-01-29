7ccb22a82d7c6d61890941fd9c6738f43d2fd0d3
thsamajiki <ant1102@naver.com>
2024-01-30 03:51:33 +0900

    2206 벽 부수고 이동하기 타인의 풀이

--- test/Graph_DFS_BFS/MainTest.java
+++ test/Graph_DFS_BFS/MainTest.java
@@ -2,20 +2,39 @@ package Graph_DFS_BFS;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class MainTest {
 
     @Test
     void solution() {
         Main main = new Main();
-//        Main.N = 9;
-//        Main.R = 5;
-//        Main.Q = 3;
-//        Main.tree = new ArrayList[Main.N + 1]; // 인접 리스트로 간선의 정보를 저장
-//        for (int i = 1; i < Main.N + 1; i++) {
-//            Main.tree[i] = new ArrayList<>();
-//        }
-//        Main.subTree = new int[Main.N + 1]; // 각 노드의 서브트리
-//        Main.visited = new boolean[Main.N + 1];
-//        assertEquals(1, main.solution(5));
+        Main.N = 6;
+        Main.M = 4;
+        Main.visited = new boolean[Main.N + 1][Main.M + 1][2];
+        Main.map = new int[][]{
+                { 0, 0, 0, 0, 0 },
+                { 0, 0, 1, 0, 0 },
+                { 0, 1, 1, 1, 0 },
+                { 0, 1, 0, 0, 0 },
+                { 0, 0, 0, 0, 0 },
+                { 0, 0, 1, 1, 1 },
+                { 0, 0, 0, 0, 0 }
+        };
+
+        assertEquals(15, main.solution(1, 1));
+
+        Main.N = 4;
+        Main.M = 4;
+        Main.visited = new boolean[Main.N + 1][Main.M + 1][2];
+        Main.map = new int[][]{
+                { 0, 0, 0, 0, 0 },
+                { 0, 0, 1, 1, 1 },
+                { 0, 1, 1, 1, 1 },
+                { 0, 1, 1, 1, 1 },
+                { 0, 1, 1, 1, 0 }
+        };
+
+        assertEquals(-1, main.solution(1, 1));
     }
 }
