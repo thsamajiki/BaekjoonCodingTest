a0b15742beae028d08aa90f1ecaa8781f86191ce
thsamajiki <ant1102@naver.com>
2024-02-02 23:48:50 +0900

    1965 상자넣기 타인의 풀이

--- test/DP_Dynamic_Programming/MainTest.java
+++ test/DP_Dynamic_Programming/MainTest.java
@@ -9,13 +9,12 @@ class MainTest {
     @Test
     void solution() {
         Main main = new Main();
-        Main.n = 6;
-        Main.stairs = new int[] { 0, 10,
-                20,
-                15,
-                25,
-                10,
-                20 };
-        assertEquals(75, main.solution(Main.stairs));
+        Main.n = 8;
+
+        assertEquals(5, main.solution(new int[] { 1, 6, 2, 5, 7, 3, 5, 6 }));
+
+        Main.n = 10;
+
+        assertEquals(10, main.solution(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 }));
     }
 }
