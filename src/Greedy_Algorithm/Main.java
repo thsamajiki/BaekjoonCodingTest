198a535cff8a4bcec91b7f2227d3f1c0d7ec5947
thsamajiki <ant1102@naver.com>
2024-02-05 02:56:23 +0900

    1339 단어 수학 나의 풀이

--- src/Greedy_Algorithm/Main.java
+++ src/Greedy_Algorithm/Main.java
@@ -4,41 +4,30 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.StringTokenizer;
-
-class Person implements Comparable<Person> {
-    int resumeRank;
-    int interviewRank;
-
-    public Person(int resumeRank, int interviewRank) {
-        this.resumeRank = resumeRank;
-        this.interviewRank = interviewRank;
-    }
-
-    @Override
-    public int compareTo(Person o) {
-        return this.resumeRank - o.resumeRank;
-    }
-}
 
 public class Main {
-    public int solution(Person[] persons) {
-        int answer = 1;
+    public int solution(String[] words) {
+        int answer = 0;
+        int[] letters = new int[26];
 
-        Arrays.sort(persons);
-//        3 2   =>      1 4                 3 6     =>      1 4                 1 4     =>      1 4
-//        1 4   =>      2 3                 7 3     =>      2 5                 2 3     =>      2 3
-//        4 1   =>      3 2                 4 2     =>      3 6                 3 2     =>      3 2
-//        2 3   =>      4 1                 1 4     =>      4 2                 4 1     =>      4 1
-//        5 5   =>      5 5                 5 7     =>      5 7                 5 7     =>      5 7
-//                                          2 5     =>      6 1                 6 6     =>      6 6
-//                                          6 1     =>      7 3                 7 5     =>      7 5
-        Person person = persons[0];
-        for (int i = 1; i < persons.length; i++) {
-            if (person.interviewRank > persons[i].interviewRank) {
-                person = persons[i];
-                answer++;
+        for(String word: words) {
+            int digit = word.length() - 1;
+
+            for(char letter: word.toCharArray()) {
+                int index = letter - 'A';
+
+                letters[index] += (int)Math.pow(10, digit);
+                digit--;
             }
+        }
+
+        Arrays.sort(letters);
+        int max = 9;
+
+        for (int i = letters.length - 1; i >= 0; i--) {
+            if (max == 0) break;
+            answer += letters[i] * max;
+            max--;
         }
 
         return answer;
@@ -49,21 +38,13 @@ public class Main {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int T = Integer.parseInt(br.readLine());
+        int N = Integer.parseInt(br.readLine());
+        String[] words = new String[N];
 
-        for (int i = 0; i < T; i++) {
-            int N = Integer.parseInt(br.readLine());
-
-            Person[] persons = new Person[N];
-
-            for (int j = 0; j < N; j++) {
-                StringTokenizer st = new StringTokenizer(br.readLine());
-                int resumeRank = Integer.parseInt(st.nextToken());
-                int interviewRank = Integer.parseInt(st.nextToken());
-                persons[j] = new Person(resumeRank, interviewRank);
-            }
-
-            System.out.println(main.solution(persons));
+        for (int i = 0; i < N; i++) {
+            words[i] = br.readLine();
         }
+
+        System.out.println(main.solution(words));
     }
 }
