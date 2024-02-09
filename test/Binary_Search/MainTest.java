6a65ef14c65ba611ed4f628351ab895a08ca9497
thsamajiki <ant1102@naver.com>
2024-02-09 23:49:34 +0900

    2805 나무 자르기 타인의 풀이

--- test/Binary_Search/MainTest.java
+++ test/Binary_Search/MainTest.java
@@ -9,11 +9,11 @@ class MainTest {
     @Test
     void solution() {
         Main main = new Main();
-        Main.N = 5;
-        Main.solutions = new int[] { -99, -2, -1, 4, 98 };
-        assertArrayEquals(new int[] { -99, 98 }, main.solution(5));
         Main.N = 4;
-        Main.solutions = new int[] { -100, -2, -1, 103 };
-        assertArrayEquals(new int[] { -100, 103 }, main.solution(4));
+        Main.M = 7;
+        assertEquals(15, main.solution(new long[] { 20, 15, 10, 17 }));
+        Main.N = 5;
+        Main.M = 20;
+        assertEquals(36, main.solution(new long[] { 4, 42, 40, 26, 46 }));
     }
 }
