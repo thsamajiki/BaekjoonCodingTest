e28fc55de7ed0a726c7dd200b71c718f510ca29b
thsamajiki <ant1102@naver.com>
2024-02-15 23:30:23 +0900

    5014 스타트링크 타인의 풀이

--- test/Graph_DFS_BFS/MainTest.java
+++ test/Graph_DFS_BFS/MainTest.java
@@ -2,39 +2,37 @@ package Graph_DFS_BFS;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 class MainTest {
 
     @Test
     void solution() {
-        Main main = new Main();
-        Main.N = 6;
-        Main.M = 4;
-        Main.visited = new boolean[Main.N + 1][Main.M + 1][2];
-        Main.map = new int[][]{
-                { 0, 0, 0, 0, 0 },
-                { 0, 0, 1, 0, 0 },
-                { 0, 1, 1, 1, 0 },
-                { 0, 1, 0, 0, 0 },
-                { 0, 0, 0, 0, 0 },
-                { 0, 0, 1, 1, 1 },
-                { 0, 0, 0, 0, 0 }
-        };
-
-        assertEquals(15, main.solution(1, 1));
-
-        Main.N = 4;
-        Main.M = 4;
-        Main.visited = new boolean[Main.N + 1][Main.M + 1][2];
-        Main.map = new int[][]{
-                { 0, 0, 0, 0, 0 },
-                { 0, 0, 1, 1, 1 },
-                { 0, 1, 1, 1, 1 },
-                { 0, 1, 1, 1, 1 },
-                { 0, 1, 1, 1, 0 }
-        };
-
-        assertEquals(-1, main.solution(1, 1));
+//        Main main = new Main();
+//        Main.N = 6;
+//        Main.M = 4;
+//        Main.visited = new boolean[Main.N + 1][Main.M + 1][2];
+//        Main.map = new int[][]{
+//                { 0, 0, 0, 0, 0 },
+//                { 0, 0, 1, 0, 0 },
+//                { 0, 1, 1, 1, 0 },
+//                { 0, 1, 0, 0, 0 },
+//                { 0, 0, 0, 0, 0 },
+//                { 0, 0, 1, 1, 1 },
+//                { 0, 0, 0, 0, 0 }
+//        };
+//
+//        assertEquals(15, main.solution(1, 1));
+//
+//        Main.N = 4;
+//        Main.M = 4;
+//        Main.visited = new boolean[Main.N + 1][Main.M + 1][2];
+//        Main.map = new int[][]{
+//                { 0, 0, 0, 0, 0 },
+//                { 0, 0, 1, 1, 1 },
+//                { 0, 1, 1, 1, 1 },
+//                { 0, 1, 1, 1, 1 },
+//                { 0, 1, 1, 1, 0 }
+//        };
+//
+//        assertEquals(-1, main.solution(1, 1));
     }
 }
