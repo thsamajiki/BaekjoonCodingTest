92f9df78b988963eccb5cc2f971317ef7b254f16
thsamajiki <ant1102@naver.com>
2024-02-17 00:05:27 +0900

    4963 섬의 개수 타인의 풀이

--- src/Graph_DFS_BFS/Main.java
+++ src/Graph_DFS_BFS/Main.java
@@ -6,24 +6,22 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int R, C, answer = 1;
-    static int[] dx = { -1, 0, 1,  0 };
-    static int[] dy = {  0, 1, 0, -1 };
-    static char[][] board;
-    static boolean[] visited;
+    static int w, h;
+    static int[] dx = { -1, -1, 0,  1, 1,  1,  0, -1 };
+    static int[] dy = {  0,  1, 1,  1, 0, -1, -1, -1 };
+    static int[][] map;
+    static boolean[][] visited;
 
-    public void solution(int x, int y, int count) {
-        for (int i = 0; i < 4; i++) {
+    public void solution(int x, int y) {
+        for (int i = 0; i < 8; i++) {
             int nextX = x + dx[i];
             int nextY = y + dy[i];
 
-            if (nextX < 0 || nextX > R - 1 || nextY < 0 || nextY > C - 1) continue;
-            if (!visited[board[nextX][nextY] - 'A']) {
-                visited[board[nextX][nextY] - 'A'] = true;
-                solution(nextX, nextY, count + 1);
-                visited[board[nextX][nextY] - 'A'] = false;
-            } else {
-                answer = Math.max(answer, count);
+            if (nextX < 0 || nextX > h - 1 || nextY < 0 || nextY > w - 1) continue;
+
+            if (!visited[nextX][nextY] && map[nextX][nextY] == 1) {
+                visited[nextX][nextY] = true;
+                solution(nextX, nextY);
             }
         }
     }
@@ -31,23 +29,37 @@ public class Main {
     public static void main(String[] args) throws IOException {
         Main main = new Main();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        R = Integer.parseInt(st.nextToken());
-        C = Integer.parseInt(st.nextToken());
-        board = new char[R][C];
-        visited = new boolean[26];
-
-        for (int i = 0; i < R; i++) {
-            String input = br.readLine();
-            for (int j = 0; j < C; j++) {
-                board[i][j] = input.charAt(j);
+        while (true) {
+            int answer = 0;
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            w = Integer.parseInt(st.nextToken());
+            h = Integer.parseInt(st.nextToken());
+            if (w == 0 && h == 0) {
+                break;
             }
+
+            map = new int[h][w];
+            visited = new boolean[h][w];
+
+            for (int i = 0; i < h; i++) {
+                st = new StringTokenizer(br.readLine());
+                for (int j = 0; j < w; j++) {
+                    map[i][j] = Integer.parseInt(st.nextToken());
+                }
+            }
+
+            for (int i = 0; i < h; i++) {
+                for (int j = 0; j < w; j++) {
+                    if (!visited[i][j] && map[i][j] == 1) {
+                        answer++;
+                        visited[i][j] = true;
+                        main.solution(i, j);
+                    }
+                }
+            }
+
+            System.out.println(answer);
         }
-
-        visited[board[0][0] - 'A'] = true;
-        main.solution(0, 0, 1);
-
-        System.out.println(answer);
     }
 }
