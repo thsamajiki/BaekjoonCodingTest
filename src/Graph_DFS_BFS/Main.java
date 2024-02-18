42afb158b887f9e7f8290987f4573637b8c38402
thsamajiki <ant1102@naver.com>
2024-02-18 18:33:57 +0900

    14502 연구소 타인의 풀이

--- src/Graph_DFS_BFS/Main.java
+++ src/Graph_DFS_BFS/Main.java
@@ -3,25 +3,84 @@ package Graph_DFS_BFS;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
+class Point {
+    int x;
+    int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
+
 public class Main {
-    static int w, h;
-    static int[] dx = { -1, -1, 0,  1, 1,  1,  0, -1 };
-    static int[] dy = {  0,  1, 1,  1, 0, -1, -1, -1 };
+    static int N, M, answer;
+    static int[] dx = { -1, 0, 1,  0 };
+    static int[] dy = {  0, 1, 0, -1 };
     static int[][] map;
     static boolean[][] visited;
 
-    public void solution(int x, int y) {
-        for (int i = 0; i < 8; i++) {
-            int nextX = x + dx[i];
-            int nextY = y + dy[i];
+    public int bfs(int[][] map) {
+        Queue<Point> q = new LinkedList<>();
 
-            if (nextX < 0 || nextX > h - 1 || nextY < 0 || nextY > w - 1) continue;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                if (map[i][j] == 2) {
+                    q.offer(new Point(i, j));
+                }
+            }
+        }
 
-            if (!visited[nextX][nextY] && map[nextX][nextY] == 1) {
-                visited[nextX][nextY] = true;
-                solution(nextX, nextY);
+        while (!q.isEmpty()) {
+            Point now = q.poll();
+
+            for (int i = 0; i < 4; i++) {
+                int nextX = now.x + dx[i];
+                int nextY = now.y + dy[i];
+
+                if (nextX < 0 || nextX > N - 1 || nextY < 0 || nextY > M - 1) continue;
+
+                if (map[nextX][nextY] == 0) {
+                    map[nextX][nextY] = 2;
+                    q.offer(new Point(nextX, nextY));
+                }
+            }
+        }
+
+        int safeAreaCount = 0;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                if (map[i][j] == 0) safeAreaCount++;
+            }
+        }
+
+        return safeAreaCount;
+    }
+
+    public void makeWall(int wallCount) {
+        if (wallCount == 3) {
+            int[][] copyMap = new int[N][M];
+            for (int i = 0; i < N; i++) {
+                copyMap[i] = map[i].clone();
+            }
+
+            answer = Math.max(answer, bfs(copyMap));
+            return;
+        }
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                if (map[i][j] == 0 && !visited[i][j]) {
+                    map[i][j] = 1;
+                    visited[i][j] = true;
+                    makeWall(wallCount + 1);
+                    map[i][j] = 0;
+                    visited[i][j] = false;
+                }
             }
         }
     }
@@ -30,36 +89,22 @@ public class Main {
         Main main = new Main();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        while (true) {
-            int answer = 0;
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            w = Integer.parseInt(st.nextToken());
-            h = Integer.parseInt(st.nextToken());
-            if (w == 0 && h == 0) {
-                break;
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        map = new int[N][M];
+        visited = new boolean[N][M];
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < M; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
             }
-
-            map = new int[h][w];
-            visited = new boolean[h][w];
-
-            for (int i = 0; i < h; i++) {
-                st = new StringTokenizer(br.readLine());
-                for (int j = 0; j < w; j++) {
-                    map[i][j] = Integer.parseInt(st.nextToken());
-                }
-            }
-
-            for (int i = 0; i < h; i++) {
-                for (int j = 0; j < w; j++) {
-                    if (!visited[i][j] && map[i][j] == 1) {
-                        answer++;
-                        visited[i][j] = true;
-                        main.solution(i, j);
-                    }
-                }
-            }
-
-            System.out.println(answer);
         }
+
+        main.makeWall(0);
+
+        System.out.println(answer);
     }
 }
