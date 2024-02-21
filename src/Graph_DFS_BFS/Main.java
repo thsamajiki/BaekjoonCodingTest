43d35cdc5abb379c11cc4892bf83e9b3ec092313
thsamajiki <ant1102@naver.com>
2024-02-21 18:50:44 +0900

    18352 특정 거리의 도시 찾기 타인의 풀이

--- src/Graph_DFS_BFS/Main.java
+++ src/Graph_DFS_BFS/Main.java
@@ -3,108 +3,73 @@ package Graph_DFS_BFS;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
-class Point {
-    int x;
-    int y;
-
-    public Point(int x, int y) {
-        this.x = x;
-        this.y = y;
-    }
-}
-
 public class Main {
-    static int N, M, answer;
-    static int[] dx = { -1, 0, 1,  0 };
-    static int[] dy = {  0, 1, 0, -1 };
-    static int[][] map;
-    static boolean[][] visited;
+    static int N, M, K ,X;
+    static ArrayList<ArrayList<Integer>> map;
+    static int[] distances;
 
-    public int bfs(int[][] map) {
-        Queue<Point> q = new LinkedList<>();
+    public void BFS() {
+        distances[X] = 0;
 
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                if (map[i][j] == 2) {
-                    q.offer(new Point(i, j));
-                }
-            }
-        }
+        Queue<Integer> q = new LinkedList<>();
+        q.offer(X);
 
         while (!q.isEmpty()) {
-            Point now = q.poll();
+            int now = q.poll();
 
-            for (int i = 0; i < 4; i++) {
-                int nextX = now.x + dx[i];
-                int nextY = now.y + dy[i];
+            for (int i = 0; i < map.get(now).size(); i++) {
+                int next = map.get(now).get(i);
 
-                if (nextX < 0 || nextX > N - 1 || nextY < 0 || nextY > M - 1) continue;
-
-                if (map[nextX][nextY] == 0) {
-                    map[nextX][nextY] = 2;
-                    q.offer(new Point(nextX, nextY));
+                if (distances[next] == -1) {
+                    distances[next] = distances[now] + 1;
+                    q.offer(next);
                 }
             }
         }
 
-        int safeAreaCount = 0;
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                if (map[i][j] == 0) safeAreaCount++;
+        boolean flag = false;
+        for (int i = 1; i <= N; i++) {
+            if (distances[i] == K) {
+                System.out.println(i);
+                flag = true;
             }
         }
 
-        return safeAreaCount;
-    }
-
-    public void makeWall(int wallCount) {
-        if (wallCount == 3) {
-            int[][] copyMap = new int[N][M];
-            for (int i = 0; i < N; i++) {
-                copyMap[i] = map[i].clone();
-            }
-
-            answer = Math.max(answer, bfs(copyMap));
-            return;
-        }
-
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                if (map[i][j] == 0 && !visited[i][j]) {
-                    map[i][j] = 1;
-                    visited[i][j] = true;
-                    makeWall(wallCount + 1);
-                    map[i][j] = 0;
-                    visited[i][j] = false;
-                }
-            }
+        if (!flag) {
+            System.out.println(-1);
         }
     }
 
     public static void main(String[] args) throws IOException {
         Main main = new Main();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         StringTokenizer st = new StringTokenizer(br.readLine());
+
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+        X = Integer.parseInt(st.nextToken());
 
-        map = new int[N][M];
-        visited = new boolean[N][M];
-
-        for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < M; j++) {
-                map[i][j] = Integer.parseInt(st.nextToken());
-            }
+        map = new ArrayList<>();
+        distances = new int[N + 1];
+        for (int i = 0; i <= N; i++) {
+            map.add(new ArrayList<>());
+            distances[i] = -1;
         }
 
-        main.makeWall(0);
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
 
-        System.out.println(answer);
+            map.get(A).add(B);
+        }
+
+        main.BFS();
     }
 }
