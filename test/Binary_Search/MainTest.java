2994df4594b0b8f910dcc90322816099f9ce32c9
thsamajiki <ant1102@naver.com>
2024-02-23 00:22:52 +0900

    2512 예산 타인의 풀이

--- test/Binary_Search/MainTest.java
+++ test/Binary_Search/MainTest.java
@@ -2,18 +2,18 @@ package Binary_Search;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class MainTest {
 
     @Test
     void solution() {
         Main main = new Main();
+
         Main.N = 4;
-        Main.M = 7;
-        assertEquals(15, main.solution(new long[] { 20, 15, 10, 17 }));
+        assertEquals(127, main.solution(new int[] { 120, 110, 140, 150 }, 485));
+
         Main.N = 5;
-        Main.M = 20;
-        assertEquals(36, main.solution(new long[] { 4, 42, 40, 26, 46 }));
+        assertEquals(100, main.solution(new int[] { 70, 80, 30, 40, 100 }, 450));
     }
 }
