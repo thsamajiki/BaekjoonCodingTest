eb6f37f812ce1397ee6ccd09812bd60238ea9bea
thsamajiki <ant1102@naver.com>
2024-02-23 00:22:48 +0900

    2512 예산 타인의 풀이

--- src/Binary_Search/Main.java
+++ src/Binary_Search/Main.java
@@ -7,44 +7,40 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int N, M; // N: 나무의 수, M: 집으로 가져가려고 하는 나무의 길이
-
-    public long solution(long[] heights) {
-        Arrays.sort(heights);
-
-        long left = 0; // 절단기 최소 높이
-        long right = heights[N - 1]; // 절단기 최대 높이
+    static int N;
+    public int solution(int[] requestArr, int M) {
+        int left = 0;
+        int right = Arrays.stream(requestArr).max().getAsInt();
 
         while (left <= right) {
-            long mid = (left + right) / 2;
-            long sum = 0;
-            
-            for(long height: heights) {
-                if (height > mid) sum += height - mid;
+            int mid = (left + right) / 2; // 상한액
+            long budget = 0; // 상한액(mid)으로 얻을 수 있는 예산
+
+            for (int i = 0; i < N; i++) {
+                if (requestArr[i] > mid) budget += mid;
+                else budget += requestArr[i];
             }
 
-            if (sum >= M) left = mid + 1;
+            if (budget <= M) left = mid + 1;
             else right = mid - 1;
         }
 
         return right;
     }
-
     public static void main(String[] args) throws IOException {
         Main main = new Main();
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(br.readLine());
 
-        long[] heights = new long[N];
-        st = new StringTokenizer(br.readLine());
+        int[] requestArr = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
-            heights[i] = Integer.parseInt(st.nextToken());
+            requestArr[i] = Integer.parseInt(st.nextToken());
         }
 
-        System.out.println(main.solution(heights));
+        int M = Integer.parseInt(br.readLine());
+
+        System.out.println(main.solution(requestArr, M));
     }
 }
