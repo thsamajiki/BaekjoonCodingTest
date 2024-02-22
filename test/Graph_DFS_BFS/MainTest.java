b36cd2b8a4b1028d339019a8f94807e4e36d4287
thsamajiki <ant1102@naver.com>
2024-02-22 22:42:42 +0900

    1520 내리막길 타인의 풀이

--- test/Graph_DFS_BFS/MainTest.java
+++ test/Graph_DFS_BFS/MainTest.java
@@ -2,37 +2,29 @@ package Graph_DFS_BFS;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class MainTest {
 
     @Test
     void solution() {
-//        Main main = new Main();
-//        Main.N = 6;
-//        Main.M = 4;
-//        Main.visited = new boolean[Main.N + 1][Main.M + 1][2];
-//        Main.map = new int[][]{
-//                { 0, 0, 0, 0, 0 },
-//                { 0, 0, 1, 0, 0 },
-//                { 0, 1, 1, 1, 0 },
-//                { 0, 1, 0, 0, 0 },
-//                { 0, 0, 0, 0, 0 },
-//                { 0, 0, 1, 1, 1 },
-//                { 0, 0, 0, 0, 0 }
-//        };
-//
-//        assertEquals(15, main.solution(1, 1));
-//
-//        Main.N = 4;
-//        Main.M = 4;
-//        Main.visited = new boolean[Main.N + 1][Main.M + 1][2];
-//        Main.map = new int[][]{
-//                { 0, 0, 0, 0, 0 },
-//                { 0, 0, 1, 1, 1 },
-//                { 0, 1, 1, 1, 1 },
-//                { 0, 1, 1, 1, 1 },
-//                { 0, 1, 1, 1, 0 }
-//        };
-//
-//        assertEquals(-1, main.solution(1, 1));
+        Main main = new Main();
+        Main.M = 4;
+        Main.N = 5;
+        Main.dp = new int[][] {
+                { -1, -1, -1, -1, -1 },
+                { -1, -1, -1, -1, -1 },
+                { -1, -1, -1, -1, -1 },
+                { -1, -1, -1, -1, -1 }
+        };
+
+        Main.map = new int[][] {
+                { 50, 45, 37, 32, 30 },
+                { 35, 50, 40, 20, 25 },
+                { 30, 30, 25, 17, 28 },
+                { 27, 24, 22, 15, 10 }
+        };
+
+        assertEquals(3, main.DFS(0, 0));
     }
 }
