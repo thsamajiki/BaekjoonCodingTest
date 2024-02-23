7013a36117b5a6b6b5c5fc29662a479f5a7e9fab
thsamajiki <ant1102@naver.com>
2024-02-23 18:27:34 +0900

    1965 상자넣기 타인의 풀이

--- test/DP_Dynamic_Programming/MainTest.java
+++ test/DP_Dynamic_Programming/MainTest.java
@@ -2,19 +2,18 @@ package DP_Dynamic_Programming;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class MainTest {
 
     @Test
     void solution() {
         Main main = new Main();
-        Main.n = 8;
 
+        Main.n = 8;
         assertEquals(5, main.solution(new int[] { 1, 6, 2, 5, 7, 3, 5, 6 }));
 
         Main.n = 10;
-
         assertEquals(10, main.solution(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 }));
     }
 }
