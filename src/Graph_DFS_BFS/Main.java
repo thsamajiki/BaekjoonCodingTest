43e5325a5189f0cf657b83f06102851d68a72f3a
thsamajiki <ant1102@naver.com>
2024-03-17 23:59:33 +0900

    16235 나무 재테크 타인의 풀이

--- src/Graph_DFS_BFS/Main.java
+++ src/Graph_DFS_BFS/Main.java
@@ -3,61 +3,127 @@ package Graph_DFS_BFS;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.PriorityQueue;
+import java.util.*;
 
-class AbsNumber implements Comparable<AbsNumber> {
-    int num;
+class Tree implements Comparable<Tree> {
+    int x;
+    int y;
+    int age;
 
-    public AbsNumber(int num) {
-        this.num = num;
+    public Tree(int x, int y, int age) {
+        this.x = x;
+        this.y = y;
+        this.age = age;
     }
 
     @Override
-    public int compareTo(AbsNumber o2) {
-        if (Math.abs(this.num) > Math.abs(o2.num)) {
-            return Math.abs(this.num) - Math.abs(o2.num);
-        } else if (Math.abs(this.num) == Math.abs(o2.num)) {
-            return this.num - o2.num;
-        } else {
-            return -1; // 무조건 -1을 주어야 하는 것이 아니고 음수이면 가능
-        }
+    public int compareTo(Tree o) {
+        return this.age - o.age;
     }
 }
 
 public class Main {
-    public String solution(int[] arr) {
-        PriorityQueue<AbsNumber> pq = new PriorityQueue<>();
+    static int N, M, K;
+    static int[] dx = { -1, -1, 0, 1, 1, 1, 0, -1 };
+    static int[] dy = { 0, 1, 1, 1, 0, -1, -1, -1 };
+    static int[][] A, map;
+    static ArrayList<Tree> trees;
+    static ArrayList<Tree> liveTrees;
+    static ArrayList<Tree> deadTrees;
 
-        StringBuilder sb = new StringBuilder();
-        for(int num: arr) {
-            if (num != 0) {
-                pq.offer(new AbsNumber(num));
+    public void spring() {
+        for (int i = 0; i < trees.size(); i++) {
+            Tree tree = trees.get(i);
+
+            // 땅에 양분이 부족해 자신의 나이만큼 양분을 먹을 수 없는 나무는 양분을 먹지 못하고 즉시 죽는다.
+            if (tree.age > map[tree.x][tree.y]) {
+                deadTrees.add(tree);
             } else {
-                if (pq.isEmpty()) {
-                    sb.append(0).append("\n");
-                } else {
-                    sb.append(pq.poll().num).append("\n");
-                }
+                map[tree.x][tree.y] -= tree.age;
+                tree.age++;
+                liveTrees.add(tree);
             }
         }
 
-        return sb.toString();
+        trees.clear();
+        trees.addAll(liveTrees);
+    }
+
+    public void summer() {
+        for (int i = 0; i < deadTrees.size(); i++) {
+            Tree tree = deadTrees.get(i);
+            map[tree.x][tree.y] += tree.age / 2;
+        }
+    }
+
+    public void autumn() {
+        for (int i = 0; i < trees.size(); i++) {
+            Tree tree = trees.get(i);
+
+            if (tree.age % 5 == 0) {
+                for (int j = 0; j < 8; j++) {
+                    int nextX = tree.x + dx[j];
+                    int nextY = tree.y + dy[j];
+
+                    if (nextX < 0 || nextX >= N || nextY < 0 || nextY >= N) continue;
+                    trees.add(new Tree(nextX, nextY, 1));
+                }
+            }
+        }
+    }
+
+    public void winter() {
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                map[i][j] += A[i][j];
+            }
+        }
     }
 
     public static void main(String[] args) throws IOException {
         Main main = new Main();
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int N = Integer.parseInt(br.readLine());
+        N = Integer.parseInt(st.nextToken()); // 땅의 가로, 세로 크기
+        M = Integer.parseInt(st.nextToken()); // 심은 나무의 개수
+        K = Integer.parseInt(st.nextToken()); // 시간(년)이 얼마나 지났는지
 
-        int[] arr = new int[N];
+        A = new int[N][N];
+        map = new int[N][N]; // 양분
+        trees = new ArrayList<>();
 
         for(int i = 0; i < N; i++) {
-            int x = Integer.parseInt(br.readLine());
-            arr[i] = x;
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                A[i][j] = Integer.parseInt(st.nextToken());
+                map[i][j] = 5;
+            }
         }
 
-        System.out.println(main.solution(arr));
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            int z = Integer.parseInt(st.nextToken());
+            trees.add(new Tree(x - 1, y - 1, z));
+        }
+
+        while (K > 0) {
+            // 구분 초기화
+            liveTrees = new ArrayList<>();
+            deadTrees = new ArrayList<>();
+            // 정렬
+            Collections.sort(trees);
+            main.spring();
+            main.summer();
+            main.autumn();
+            main.winter();
+            K--;
+        }
+
+        System.out.println(trees.size());
     }
 }
+
