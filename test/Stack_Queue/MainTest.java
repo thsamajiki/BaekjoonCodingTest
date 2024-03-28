f31f6473ebfa1e01e6e14c4ee0fb2ff7b320748c
thsamajiki <ant1102@naver.com>
2024-03-28 22:24:56 +0900

    2493 탑 타인의 풀이

--- test/Stack_Queue/MainTest.java
+++ test/Stack_Queue/MainTest.java
@@ -1,12 +1,15 @@
 package Stack_Queue;
 
+
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class MainTest {
 
     @Test
     void solution() {
         Main main = new Main();
-//        assertEquals("<3, 6, 2, 7, 5, 1, 4>", main.solution(7, 3));
+        assertEquals("0 0 2 2 4 ", main.solution(5, new int[] {0, 6, 9, 5, 7, 4}));
     }
 }
