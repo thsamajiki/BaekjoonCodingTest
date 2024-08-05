9987df1c82315b6c0f663665e515663cf0067f1d
thsamajiki <ant1102@naver.com>
2024-08-05 14:20:12 +0900

    11403 경로 찾기 타인의 풀이

--- src/Graph_DFS_BFS/Main.java
+++ src/Graph_DFS_BFS/Main.java
@@ -1,70 +1,41 @@
 package Graph_DFS_BFS;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
 public class Main {
-    static int N, M, V;
-    static int[][] graph;
-    static boolean[] visited;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-    public static void DFS(int start) {
-        visited[start] = true;
-        System.out.print(start + " ");
+        int N = Integer.parseInt(br.readLine());
 
-        for (int i = 1; i <= N; i++) {
-            if (graph[start][i] == 1 && !visited[i]) {
-                DFS(i);
+        int[][] graph = new int[N][N];
+
+        for(int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for(int j = 0; j < N; j++) {
+                graph[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-    }
 
-    public static void BFS(int start) {
-        Queue<Integer> q = new LinkedList<>();
-        visited = new boolean[N + 1];
-        visited[start] = true;
-        q.offer(start);
-
-        while (!q.isEmpty()) {
-            int now = q.poll();
-
-            System.out.print(now + " ");
-
-            for (int i = 1; i <= N; i++) {
-                if (graph[now][i] == 1 && !visited[i]) {
-                    visited[i] = true;
-                    q.offer(i);
+        for(int k = 0; k < N; k++) {
+            for(int i = 0; i < N; i++) {
+                for(int j = 0; j < N; j++) {
+                    if(graph[i][k] == 1 && graph[k][j] == 1) {
+                        graph[i][j] = 1;
+                    }
                 }
             }
         }
-    }
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        N = Integer.parseInt(st.nextToken()); // 정점의 개수
-        M = Integer.parseInt(st.nextToken()); // 간선의 개수
-        V = Integer.parseInt(st.nextToken()); // 탐색을 시작할 정점의 번호
-
-        graph = new int[N + 1][N + 1];
-        visited = new boolean[N + 1];
-
-        for (int i = 0; i < M; i++) {
-            st = new StringTokenizer(br.readLine());
-            int start = Integer.parseInt(st.nextToken());
-            int end = Integer.parseInt(st.nextToken());
-
-            graph[start][end] = 1;
-            graph[end][start] = 1;
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < N; i++) {
+            for(int j = 0; j < N; j++) {
+                sb.append(graph[i][j]).append(" ");
+            }
+            sb.append("\n");
         }
 
-        DFS(V);
-        System.out.println();
-        BFS(V);
+        System.out.println(sb);
     }
 }
